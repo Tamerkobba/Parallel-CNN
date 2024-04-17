c81d243c3ec6a82f7e8285ed5533e03f75743540
Tamerkobba <126768205+Tamerkobba@users.noreply.github.com>
2024-04-17 12:10:52 +0300

    Printing running time

--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <chrono>
 #include "byteswap.h"
 #include "CNN/cnn.h"
 
@@ -107,50 +108,45 @@ vector<case_t> read_test_cases()
 
 	return cases;
 }
-
 int main()
 {
-	vector<case_t> cases = read_test_cases();
+    using namespace std::chrono;  // Use the namespace for easy access to chrono functions
+    auto start = high_resolution_clock::now();  // Start timer
 
-	vector<layer_t*> layers;
+    vector<case_t> cases = read_test_cases();
 
-	conv_layer_t * layer1 = new conv_layer_t( 1, 5, 8, cases[0].data.size );		// 28 * 28 * 1 -> 24 * 24 * 8
-	relu_layer_t * layer2 = new relu_layer_t( layer1->out.size );
-	pool_layer_t * layer3 = new pool_layer_t( 2, 2, layer2->out.size );				// 24 * 24 * 8 -> 12 * 12 * 8
-	fc_layer_t * layer4 = new fc_layer_t(layer3->out.size, 10);					// 4 * 4 * 16 -> 10
+    vector<layer_t*> layers;
+    conv_layer_t *layer1 = new conv_layer_t(1, 5, 8, cases[0].data.size);
+    relu_layer_t *layer2 = new relu_layer_t(layer1->out.size);
+    pool_layer_t *layer3 = new pool_layer_t(2, 2, layer2->out.size);
+    fc_layer_t *layer4 = new fc_layer_t(layer3->out.size, 10);
 
-	layers.push_back( (layer_t*)layer1 );
-	layers.push_back( (layer_t*)layer2 );
-	layers.push_back( (layer_t*)layer3 );
-	layers.push_back( (layer_t*)layer4 );
+    layers.push_back((layer_t*)layer1);
+    layers.push_back((layer_t*)layer2);
+    layers.push_back((layer_t*)layer3);
+    layers.push_back((layer_t*)layer4);
 
+    float amse = 0;
+    int ic = 0;
 
+    for (long ep = 0; ep < 100000;)
+    {
+        for (case_t& t : cases)
+        {
+            float xerr = train(layers, t.data, t.out);
+            amse += xerr;
 
-	float amse = 0;
-	int ic = 0;
+            ep++;
+            ic++;
 
-	for ( long ep = 0; ep < 100000; )
-	{
+            if (ep % 1000 == 0)
+                cout << "case " << ep << " err=" << amse/ic << endl;
+        }
+    }
 
-		for ( case_t& t : cases )
-		{
-			float xerr = train( layers, t.data, t.out );
-			amse += xerr;
-
-			ep++;
-			ic++;
-
-			if ( ep % 1000 == 0 )
-				cout << "case " << ep << " err=" << amse/ic << endl;
-
-			// if ( GetAsyncKeyState( VK_F1 ) & 0x8000 )
-			// {
-			//	   printf( "err=%.4f%\n", amse / ic  );
-			//	   goto end;
-			// }
-		}
-	}
-	// end:
+    auto stop = high_resolution_clock::now();  // Stop timer
+    auto duration = duration_cast<seconds>(stop - start);  // Calculate duration
+    cout << "Time taken by function: " << duration.count() << " seconds" << endl;
 
 
 
