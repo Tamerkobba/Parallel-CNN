4c665f3f41dfaf850bd0536b1ceaea28be277a4d
Tamerkobba <126768205+Tamerkobba@users.noreply.github.com>
2024-04-30 19:43:46 +0300

    update

--- Openmp/Main.cpp
+++ Openmp/Main.cpp
@@ -1,6 +1,6 @@
 #define USE_MNIST_LOADER
 #define MNIST_DOUBLE
-#include "mnist.h"
+#include "mnist.h" 
 #include "layer.h"
 #include <cstdio>
 #include <ctime>
@@ -8,6 +8,7 @@
 #include <cmath>
 #include <memory>
 #include <time.h>
+    double total_convolution_time = 0, total_pooling_time = 0, total_fully_connected_time = 0,total_gradient_time=0;
 
 static mnist_data *train_set, *test_set;
 static unsigned int train_cnt, test_cnt;
@@ -41,11 +42,16 @@ static inline void loaddata()
 }
 
 int main(int argc, const char **argv) {
+    
     srand(time(NULL));
-
     loaddata();
     learn();
     test();
+
+    printf("Total Convolution Time: %f ms\n", total_convolution_time);
+    printf("Total Pooling Time: %f ms\n", total_pooling_time);
+    printf("Total Fully Connected Time: %f ms\n", total_fully_connected_time);
+    printf("Total Time on applying gradients: %f ms\n", total_gradient_time);
 
     return 0;
 }
@@ -63,88 +69,118 @@ static double forward_pass(double data[28][28]) {
 	l_c1.clear();
 	l_s1.clear();
 	l_f.clear();
-
+    float milliseconds=0;
 	clock_t start, end;
-	start = clock();
-
-	l_input.setOutput((float *)input);
+    clock_t start_1, end_1;
+    start_1=clock();
 	
 
-    fp_preact_c1((float (*)[28])l_input.output, (float (*)[24][24])l_c1.preact, (float (*)[5][5])l_c1.weight);
-    fp_bias_c1((float (*)[24][24])l_c1.preact, l_c1.bias);
+	l_input.setOutput((float *)input);
+	 // forward pass Convolution Layer
+    start = clock();
+    fp_c1((float (*)[28])l_input.output, (float (*)[24][24])l_c1.preact, (float (*)[5][5])l_c1.weight,l_c1.bias);
     apply_step_function(l_c1.preact, l_c1.output, l_c1.O);
+    end = clock();
+    milliseconds = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    total_convolution_time += milliseconds;
 
-    fp_preact_s1((float (*)[24][24])l_c1.output, (float (*)[6][6])l_s1.preact, (float (*)[4][4])l_s1.weight);
-    fp_bias_s1((float (*)[6][6])l_s1.preact, l_s1.bias);
+     // forward pass pooling Layer
+    start = clock();
+    fp_s1((float (*)[24][24])l_c1.output, (float (*)[6][6])l_s1.preact, (float (*)[4][4])l_s1.weight,l_s1.bias);
     apply_step_function(l_s1.preact, l_s1.output, l_s1.O);
+    end = clock();
+    milliseconds = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    total_pooling_time += milliseconds;
 
+ // forward pass Fully Connected Layer
+    start = clock();
     fp_preact_f((float (*)[6][6])l_s1.output, l_f.preact, (float (*)[6][6][6])l_f.weight);
     fp_bias_f(l_f.preact, l_f.bias);
     apply_step_function(l_f.preact, l_f.output, l_f.O);
-
     end = clock();
-    return ((double)(end - start)) / CLOCKS_PER_SEC;
+    milliseconds = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    total_fully_connected_time += milliseconds;
+end_1= clock();
+	return ((double) (end_1 - start_1)) / CLOCKS_PER_SEC;
 }
 
 static double back_pass() {
-    clock_t start = clock();
-
+    clock_t start,end;
+     clock_t start_1,end_1;
+     start_1=clock();
+   
+ float milliseconds=0;
+start = clock();
     bp_weight_f((float (*)[6][6][6])l_f.d_weight, l_f.d_preact, (float (*)[6][6])l_s1.output);
     bp_bias_f(l_f.bias, l_f.d_preact);
-
+   end = clock();
+    milliseconds = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    total_fully_connected_time += milliseconds;
+    start = clock();
     bp_output_s1((float (*)[6][6])l_s1.d_output, (float (*)[6][6][6])l_f.weight, l_f.d_preact);
     bp_preact_s1((float (*)[6][6])l_s1.d_preact, (float (*)[6][6])l_s1.d_output, (float (*)[6][6])l_s1.preact);
     bp_weight_s1((float (*)[4][4])l_s1.d_weight, (float (*)[6][6])l_s1.d_preact, (float (*)[24][24])l_c1.output);
     bp_bias_s1(l_s1.bias, (float (*)[6][6])l_s1.d_preact);
-
+       end = clock();
+    milliseconds = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    total_pooling_time += milliseconds;
+start = clock();
     bp_output_c1((float (*)[24][24])l_c1.d_output, (float (*)[4][4])l_s1.weight, (float (*)[6][6])l_s1.d_preact);
     bp_preact_c1((float (*)[24][24])l_c1.d_preact, (float (*)[24][24])l_c1.d_output, (float (*)[24][24])l_c1.preact);
     bp_weight_c1((float (*)[5][5])l_c1.d_weight, (float (*)[24][24])l_c1.d_preact, (float (*)[28])l_input.output);
     bp_bias_c1(l_c1.bias, (float (*)[24][24])l_c1.d_preact);
-
+end = clock();
+    milliseconds = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    total_convolution_time += milliseconds;
+    start = clock();
 	apply_grad(l_f.weight, l_f.d_weight, l_f.M * l_f.N);
 	apply_grad(l_s1.weight, l_s1.d_weight, l_s1.M * l_s1.N);
 	apply_grad(l_c1.weight, l_c1.d_weight, l_c1.M * l_c1.N);
-    clock_t end = clock();
-    return ((double)(end - start)) / CLOCKS_PER_SEC;
+    end = clock();
+    milliseconds = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    total_gradient_time += milliseconds;
+end_1= clock();
+	return ((double) (end_1- start_1)) / CLOCKS_PER_SEC;
 }
 
 static void learn() {
     float err;
-    int iter = 1;
-    double time_taken = 0.0;
+	int iter = 1;
+	
+	double time_taken = 0.0;
 
-    fprintf(stdout, "Learning\n");
+	fprintf(stdout ,"Learning\n");
 
-    while (iter < 0 || iter-- > 0) {
-        err = 0.0f;
+	while (iter < 0 || iter-- > 0) {
+		err = 0.0f;
 
-        for (int i = 0; i < train_cnt; ++i) {
-            float tmp_err;
+		for (int i = 0; i < train_cnt; ++i) {
+			float tmp_err;
 
-            time_taken += forward_pass(train_set[i].data);
+			time_taken += forward_pass(train_set[i].data);
 
-            l_f.bp_clear();
-            l_s1.bp_clear();
-            l_c1.bp_clear();
+			l_f.bp_clear();
+			l_s1.bp_clear();
+			l_c1.bp_clear();
 
             // Euclid distance of train_set[i]
     makeError(l_f.d_preact, l_f.output, train_set[i].label, 10);
             tmp_err = vectorNorm(l_f.d_preact, 10);
             err += tmp_err;
-            time_taken += back_pass();
+           time_taken += back_pass();
         }
 
         err /= train_cnt;
-        fprintf(stdout, "error: %e, time_on_cpu(sequential): %lf\n", err, time_taken);
+		fprintf(stdout, "error: %e, time_on_cpu: %lf\n", err, time_taken);
 
-        if (err < threshold) {
-            fprintf(stdout, "Training complete, error less than threshold\n\n");
-            break;
-        }
-    }
+		if (err < threshold) {
+			fprintf(stdout, "Training complete, error less than threshold\n\n");
+			break;
+		}
 
-    fprintf(stdout, "\nTime - %lf\n", time_taken);
+	}
+	
+	fprintf(stdout, "\n Time - %lf\n", time_taken);
 }
 
 static unsigned int classify(double data[28][28]) {
